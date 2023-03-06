ef75379ba9a6ca5549e33ba15c38bb78d758baa3
swastikamishra <swastikamishra18@gmail.com>
2023-03-06 19:50:17 +0530

    project3

---
+++ subjectdependency/src/main/java/subject/dependency/SubjectdependencyApplication.java
@@ -0,0 +1,13 @@
+package subject.dependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectdependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectdependencyApplication.class, args);
+	}
+
+}
---
+++ subjectdependency/src/test/java/subject/dependency/SubjectdependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package subject.dependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectdependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
