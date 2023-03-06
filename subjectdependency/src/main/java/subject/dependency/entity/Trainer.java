4c4c0b18bf26d8255031467119734f82f6180a2e
swast <swast@DESKTOP-9EMH22P>
2023-03-06 18:59:22 +0530

    Project3

---
+++ subjectdependency/src/main/java/subject/dependency/entity/Trainer.java
@@ -0,0 +1,37 @@
+package subject.dependency.entity;
+
+public class Trainer {
+	
+	private Subject subject;
+
+	public Trainer() {
+		super();
+		
+	}
+
+	public Trainer(Subject subject) {
+		super();
+		this.subject = subject;
+	}
+
+	
+	
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public String toString() {
+		return "Trainer [subject=" + subject + "]";
+	}
+
+	public void teach ()
+	{
+		System.out.println("Trainer is teaching "+ subject.getName());
+	}
+
+}
