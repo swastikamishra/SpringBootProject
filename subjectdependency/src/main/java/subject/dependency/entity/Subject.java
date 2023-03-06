2688b40d83f4f61b786ddfc100d6ed57e108a858
swastikamishra <swastikamishra18@gmail.com>
2023-03-06 21:18:07 +0530

    Project3

---
+++ subjectdependency/src/main/java/subject/dependency/entity/Subject.java
@@ -0,0 +1,22 @@
+package subject.dependency.entity;
+
+public class Subject {
+	
+private String name;
+
+public Subject(String name) {
+	super();
+	this.name = name;
+}
+
+public String getName() {
+	return name;
+}
+
+
+	
+	
+	
+	
+
+}
