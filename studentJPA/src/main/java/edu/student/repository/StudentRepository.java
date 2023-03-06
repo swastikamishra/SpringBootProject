86fad16ae0d8471c8acba9c29ba527cb8e0e0572
Swastika Mishra <115781825+swastikamishra@users.noreply.github.com>
2023-03-06 20:46:10 +0530

    Delete studentJPA directory

--- studentJPA/src/main/java/edu/student/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.student.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.student.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
