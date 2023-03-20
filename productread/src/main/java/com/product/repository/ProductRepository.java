0d3c7cd8cfb9192ee0cbc2abf6d88dfe3860ca04
Swastika Mishra <115781825+swastikamishra@users.noreply.github.com>
2023-03-20 23:45:05 +0530

    Delete productread directory

--- productread/src/main/java/com/product/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.product.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
